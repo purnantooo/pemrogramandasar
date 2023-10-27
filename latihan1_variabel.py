6daa306ccfa2115a1328840399842a426ea08972
purnantooo <149077000+purnantooo@users.noreply.github.com>
2023-10-27 19:35:36 +0700

    Create latihan1_variabel.py

---
+++ latihan1_variabel.py
@@ -0,0 +1,35 @@
+a = 9
+b = 6
+c = 4
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("penyelesaian:",(a+b)*(a-c))
+
+
+a = 11
+b = 8
+c = 6
+print("a:",a)
+print("b:",b)
+print("c:",c)
+print("penyelesaian:",(a*b)/(a+c))
+
+
+a = 10
+b = 4
+t = 6
+print("a:",a)
+print("b:",b)
+print("t:",t) 
+print("penyelesaian:",1/2*(a+b)*t) 
+
+
+r = 7
+print("r:",r)
+print("keliling lingkaran adalah:",2*3.14*7)
+
+
+r = 9
+print("r:",r)
+print("Luas lingkaran adalah:",3.14*9*9)
