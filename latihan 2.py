3f0ea07f838eee6b1cf127e2687748337c9348cf
purnantooo <149077000+purnantooo@users.noreply.github.com>
2023-11-03 19:51:13 +0700

    Create latihan 2.py

---
+++ latihan 2.py
@@ -0,0 +1,13 @@
+#inputan biodata 
+
+nim = input("masukan nim:")
+nama=input("masukan nama:")
+kelas=input("masukan kelas:")
+prodi=input("masukan prodi:")
+print("==================")
+print(" biodata mahasiswa ")
+print("=============")
+print("nim:",nim)
+print("nama:",nama)
+print("kelas:",kelas)
+print("prodi:",prodi)
